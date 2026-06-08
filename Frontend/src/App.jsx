b17b07449263a15feb28c54bebee808eaf18e270
jasimjuwaith <jasimjuwaith783@gmail.com>
2026-06-08 12:01:21 +0530

    Implemented main files

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+﻿import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import AppRouter from './routes/AppRouter'
+import ErrorBoundary from './components/ErrorBoundary'
+
+function App() {
+  const darkMode = useSelector(s => s.ui.darkMode)
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [darkMode])
+
+  return (
+    <ErrorBoundary>
+      <AppRouter />
+    </ErrorBoundary>
+  )
+}
+
+export default App
